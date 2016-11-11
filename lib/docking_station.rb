30cd1377030a8de0d85357a07eb575744e3af949
Pea M. Tyczynska <pea@Peas-MacBook-Pro.local>
2016-11-11 12:10:13 +0000

    Working with Elisabeth, section 15 on single responsibility principle, done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,23 +2,32 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike
+
   def initialize
     @bikes = []
   end
 
   def release_bike
-    raise "No bikes available" if @bikes == []
+    raise "No bikes available" if empty?
     bike = @bikes.last
     @bikes.pop
     bike
   end
 
   def dock(bike)
-    raise "Docking station full" if @bikes.size == 20
+    raise "Docking station full" if full?
     @bikes << bike
     bike
   end
 
-attr_reader :bike
+private
+def full?
+  @bikes.size >= 20
+end
+
+def empty?
+  @bikes == []
+end
 
 end
