c4b94822aa872c0d4676b1a92f02644d24095767
ejatkin <ejatkin@gmail.com>
2016-11-11 10:26:00 +0000

    Working with Pea - completed exercise 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,15 +3,14 @@ require_relative 'bike'
 class DockingStation
 
   def release_bike
-    Bike.new
+    raise "No bikes available" if @bike == nil
+    @bike
   end
 
   def dock(bike)
     @bike = bike
   end
 
-#  def dock(bike)
-#    puts "This is a bike!: #{@bike}"
-#  end
+attr_reader :bike
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,21 +5,24 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
   it "gets a bike" do
-    expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
+    subject.dock(Bike.new)
+    expect(subject.release_bike).to be_an_instance_of(Bike)
   end
 
   it "checks it is working" do
-    bike = DockingStation.new.release_bike
+    bike = subject.dock(Bike.new)
     expect(bike).to be_working
   end
 
  it "docking station docks bikes" do
-   bike = DockingStation.new.release_bike
+   bike = subject.dock(Bike.new)
    docked_bike = subject.dock(bike)
  end
 
-it "sees if a bike has been docked" do
-  
+it { is_expected.to respond_to(:bike) }
+
+it "doesn't release a bike if none are available" do
+  expect { DockingStation.new.release_bike }.to raise_error("No bikes available")
 end
 
 end
